15e360b98e679bc7537aabff916ac9703e0fa6e0
OumarIKeita <22414516@student.ciu.edu.tr>
2026-08-18 14:51:44 -0400

    Configure json

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux' // Import Provider
-import store from './store.js'         // Import the store you just created
+import store from './store.js'         // import store.js component
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
