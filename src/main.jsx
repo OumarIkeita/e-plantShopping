425aa12d1f9c637ba5c3ddfeda34977ba693f009
OumarIKeita <22414516@student.ciu.edu.tr>
2026-08-18 14:39:58 -0400

    Configured Redux store and connected cartReducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import { Provider } from 'react-redux' // Import Provider
+import store from './store.js'         // Import the store you just created
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Wrap the App component in the Provider and pass the store */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
--- src/store.js
+++ src/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+/**
+ * Redux Store Configuration
+ * 
+ * configureStore handles:
+ * 1. Combining reducers (even if we only have one)
+ * 2. Adding the Redux Thunk middleware by default
+ * 3. Enabling the Redux DevTools extension automatically
+ */
+const store = configureStore({
     reducer: {
+        // This maps the 'cart' state to the logic defined in CartSlice.jsx
         cart: cartReducer,
     },
 });
-export default store
+
+export default store;
